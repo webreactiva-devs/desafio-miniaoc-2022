5d000a8fb5ff385e9f0bccda4140cc7421164b9b
sergioedo <sergio.edo@gmail.com>
2022-11-24 19:15:58 +0100

    fix basic test with char 'A'

--- soluciones/sergioedo/reto-3/index.test.js
+++ soluciones/sergioedo/reto-3/index.test.js
@@ -1,18 +1,17 @@
 import { expect, test } from 'vitest'
 import { grid2Char } from 'index'
 
-test('Test grid conversion to character', () => {
+test('Test grid conversion to character A', () => {
     const input = [
         [1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
     ]
     const expectedOutput = 'A'
 
     expect(grid2Char(input)).toBe(expectedOutput)
 })
-
