cd81a29d320d544f832b574e025a245eb183b752
sergioedo <sergio.edo@gmail.com>
2022-11-24 17:25:39 +0100

    reto3 - test grid to char

---
+++ soluciones/sergioedo/reto-3/index.js
@@ -0,0 +1,2 @@
+export const grid2Char = (grid) => {
+}
---
+++ soluciones/sergioedo/reto-3/index.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from 'vitest'
+import { grid2Char } from 'index'
+
+test('Test grid conversion to character', () => {
+    const input = [
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+    ]
+    const expectedOutput = 'A'
+
+    expect(grid2Char(input)).toBe(expectedOutput)
+})
+
