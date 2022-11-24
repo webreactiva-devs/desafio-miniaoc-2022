0b13251d7962cbfe30535f26e3b9d770b90be385
sergioedo <sergio.edo@gmail.com>
2022-11-24 19:13:38 +0100

    basic test passed

--- soluciones/sergioedo/reto-3/index.js
+++ soluciones/sergioedo/reto-3/index.js
@@ -1,2 +1,15 @@
+const char2Grid = {
+    'A': [
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+    ]
+}
+
 export const grid2Char = (grid) => {
+    return Object.keys(char2Grid).find(char => char2Grid[char].toString() === grid.toString())
 }
