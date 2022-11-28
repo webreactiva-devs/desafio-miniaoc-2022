8fcf511fdab9c494e1346774a57c1e42a7120b50
Borja Quevedo <borjota@gmail.com>
2022-11-28 22:16:10 +0100

    Quick Caesar implementation

--- soluciones/niltsiar/reto-1/src/main/kotlin/Main.kt
+++ soluciones/niltsiar/reto-1/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+const val hiddenMessage = "NVI EPVI YZ BVUOZGPBVOSZ"
+
 fun main() {
-    println("Hello WR!")
+    println(decrypt(hiddenMessage, 5))
+}
+
+fun decrypt(encryptedMessage: String, key: Int): String {
+
+    require(encryptedMessage.all { it.isLetter() || it.isWhitespace() })
+
+    return encryptedMessage
+        .uppercase()
+        .split(" ")
+        .joinToString(" ") {
+            it.map { char ->
+                val newChar = char + (key % 26)
+                if (newChar.code >= 91) newChar - 26
+                else if (newChar.code < 65) newChar + 26
+                else newChar
+            }.joinToString("")
+        }
 }
--- soluciones/niltsiar/reto-1/src/test/kotlin/Specs.kt
+++ soluciones/niltsiar/reto-1/src/test/kotlin/Specs.kt
@@ -1,9 +1,57 @@
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
-class MyFirstTestClass : StringSpec({
+class Specs : StringSpec({
 
-    "my first test" {
-        1 + 2 shouldBe 3
+    "Letters are ciphered properly" {
+        val input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val expectecOuput = "BCDEFGHIJKLMNOPQRSTUVWXYZA"
+
+        decrypt(input, 1) shouldBe expectecOuput
+    }
+
+    "lowercase letters are treated as uppercase letters" {
+        val input = "abcdefghijklmnopqrstuvwxyz"
+        val expectecOuput = "BCDEFGHIJKLMNOPQRSTUVWXYZA"
+
+        decrypt(input, 1) shouldBe expectecOuput
+    }
+
+    "if input is not letters throw exception" {
+        listOf("1", "@", "!")
+            .forAll { input ->
+                shouldThrow<IllegalArgumentException> {
+                    decrypt(input, 1)
+                }
+            }
+    }
+
+    "spaces are kept" {
+        val input = "ABC DEF"
+        val expectedOutput = "BCD EFG"
+        decrypt(input, 1) shouldBe expectedOutput
+    }
+
+    "Key works for numbers bigger that the number of letters" {
+        val input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val expectecOuput = "BCDEFGHIJKLMNOPQRSTUVWXYZA"
+
+        decrypt(input, 27) shouldBe expectecOuput
+    }
+
+    "Key works for negative numbers" {
+        val input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val expectecOuput = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
+
+        decrypt(input, -1) shouldBe expectecOuput
+    }
+
+    "Key works for more negative numbers" {
+        val input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val expectecOuput = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
+
+        decrypt(input, -27) shouldBe expectecOuput
     }
 })
