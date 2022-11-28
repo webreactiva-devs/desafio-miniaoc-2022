7caea739f34e841791ac413c54c9c45c88b0d33c
Borja Quevedo <borjota@gmail.com>
2022-11-28 22:52:21 +0100

    Make network call

--- soluciones/niltsiar/reto-1/build.gradle.kts
+++ soluciones/niltsiar/reto-1/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
+    application
 }
 
 repositories {
@@ -16,7 +18,16 @@ tasks {
     }
 }
 
+application {
+    mainClass.set("MainKt")
+}
+
 dependencies {
+    implementation("io.ktor:ktor-client-core:2.1.3")
+    implementation("io.ktor:ktor-client-cio:2.1.3")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
+
     testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
     testImplementation("io.kotest:kotest-assertions-core:5.5.4")
 }
--- soluciones/niltsiar/reto-1/src/main/kotlin/Main.kt
+++ soluciones/niltsiar/reto-1/src/main/kotlin/Main.kt
@@ -1,7 +1,50 @@
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
 const val hiddenMessage = "NVI EPVI YZ BVUOZGPBVOSZ"
 
-fun main() {
-    println(decrypt(hiddenMessage, 5))
+@Serializable
+data class Request(
+    val solution: String,
+)
+
+@Serializable
+data class Response(
+    val status: String,
+    @SerialName("message_to_telegram")
+    val messagesToTelegram: String,
+    @SerialName("supercoco_is_here")
+    val whereIsSuperCoco: String,
+    @SerialName("next_challenge")
+    val nextChallenge: String,
+)
+
+suspend fun main() {
+    val url = "https://donde-esta-supercoco-delineas.vercel.app/api/reto/1"
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+    val response = client.post(url) {
+        contentType(ContentType.Application.Json)
+        setBody(Request(decrypt(hiddenMessage, 5)))
+    }
+    println(response.body<Response>())
+    client.close()
 }
 
 fun decrypt(encryptedMessage: String, key: Int): String {
